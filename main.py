78dad9778d84d6317f914e14e775a91c1640ce9e
Shashwat Singh <shashwats17@gmail.com>
2024-06-24 13:10:16 -0400

    case handled

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
-
-try:
-    print("Hello my name is", sys.argv[1 ])
-except IndexError:
-    print("Invalid # of arguments")
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("Hello" , sys.argv[1])
