b97ce2348e9e0b4dafc0eaa4e274d31a07bf23f2
Shashwat Singh <shashwats17@gmail.com>
2024-06-26 16:06:08 -0400

    API

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import cowsay
+import json
+import requests
 import sys
 
-if len(sys.argv) == 2:
-    cowsay.trex("Hello, " + sys.argv[1])
+
+if len(sys.argv) != 2:
+    sys.exit
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+
+print(json.dumps(response.json(), indent=2))
